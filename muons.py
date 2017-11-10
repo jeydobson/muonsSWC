49dc06e634a03e3b638d83e56460d141fbda96a9
Jim Dobson <j.dobson@ucl.ac.uk>
2017-11-10 12:14:17 +0000

    Add simple event loop that prints when have two leptons

--- muons.py
+++ muons.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python
 from ROOT import TChain
 data = TChain("mini")
-data.Add("http://opendata.atlas.cern.release/samples/Data/DataMuons.root")
+data.Add("/home/jdobson/SoftwareCarpentry/DataMuons.root")
+
+num_events = data.GetEntries()
+print("Number of events = "+str(num_events))
+
+num_events_to_process = 10000 # for testing
+for i_event in range(num_events_to_process):
+    data.GetEntry(i_event)
+    n_leptons = data.lep_n
+    if n_leptons > 2:
+        print("Number of leptons = "+str(n_leptons))
+
