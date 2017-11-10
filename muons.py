56d78e4de28e09eed8d696feeaaf2aaa45d2d077
Jim Dobson <j.dobson@ucl.ac.uk>
2017-11-10 12:38:07 +0000

    Create TLotentzVectors and check can recover Pt

--- muons.py
+++ muons.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from ROOT import TChain
+from ROOT import TChain, TLorentzVector
 data = TChain("mini")
 data.Add("/home/jdobson/SoftwareCarpentry/DataMuons.root")
 
@@ -16,4 +16,8 @@ for i_event in range(num_events_to_process):
         pt1 = data.lep_pt[0]
         pt2 = data.lep_pt[1]
         print("Lepton Pts are: {} and {}".format(pt1, pt2))
-
+        p1 = TLorentzVector()
+        p1.SetPtEtaPhiE(data.lep_pt[0], data.lep_eta[0], data.lep_phi[0], data.lep_E[0])        
+        p2 = TLorentzVector()
+        p2.SetPtEtaPhiE(data.lep_pt[1], data.lep_eta[1], data.lep_phi[1], data.lep_E[1])
+        print("Pts from TLorentzVector are {} and {}".format(p1.Pt(), p2.Pt())) 
